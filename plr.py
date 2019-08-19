3c56d2468a4eadf499c198649f06daaf07378221
nanguo.sjh <nanguo.sjh@alibaba-inc.com>
2019-08-19 13:54:34 +0800

    updated

--- plr.py
+++ plr.py
@@ -17,7 +17,7 @@ def pod_actual_name_in_line(line_str: str):
 def path_from_abstract_name(abstract_name: str):
     result = os.listdir("..")
     for file_name in result:
-        if abstract_name in file_name.lower():
+        if abstract_name.lower() in file_name.lower():
             return file_name
     return None
 
@@ -28,9 +28,6 @@ def generate_local_ref_str(actual_name: str, path: str):
 
 
 def replace_string_in_podfile(abstract_name: str) -> None:
-    local_path_str = path_from_abstract_name(abstract_name)
-    if local_path_str is None:
-        print('Did not find local path named ' + abstract_name + ' in ../')
     with open('Podfile') as f:
         s = f.read()
     components = s.split('\n')
@@ -39,10 +36,12 @@ def replace_string_in_podfile(abstract_name: str) -> None:
         if abstract_name in line_str.lower():
             actual_name = pod_actual_name_in_line(line_str)
             if actual_name is not None:
-                local_ref_str = generate_local_ref_str(actual_name=actual_name, path=local_path_str)
-                components[index] = local_ref_str
-                replacing_action_occurred = True
-                print('Replaced to local ref: ' + actual_name)
+                local_path_str = path_from_abstract_name(actual_name)
+                if local_path_str is not None:
+                    local_ref_str = generate_local_ref_str(actual_name=actual_name, path=local_path_str)
+                    components[index] = local_ref_str
+                    replacing_action_occurred = True
+                    print('Replaced to local ref: ' + actual_name)
     if replacing_action_occurred:
         content = '\n'.join(components)
         with open('Podfile', 'w') as f:

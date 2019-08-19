ef981761241bea2fc895f7e858ab46736c4cd838
nanguo.sjh <nanguo.sjh@alibaba-inc.com>
2019-08-19 14:16:57 +0800

    install script updated

--- easy_install.sh
+++ easy_install.sh
@@ -4,7 +4,17 @@ rm -rf '.pod_local_replacing'
 git clone https://github.com/OpenFibers/pod_local_replacing.git pod_local_replacing
 mv pod_local_replacing .pod_local_replacing
 cd -
-echo "" >> ~/.zshrc
-echo "alias plr='python3 ~/.pod_local_replacing/plr.py'" >> ~/.zshrc
-echo "" >> ~/.bash_profile
-echo "alias plr='python3 ~/.pod_local_replacing/plr.py'" >> ~/.bash_profile
+
+if grep -q "alias plr='python3 ~/.pod_local_replacing/plr.py'" ~/.zshrc; then
+  echo 'zshrc alias has already been added'
+else
+  echo "" >> ~/.zshrc
+  echo "alias plr='python3 ~/.pod_local_replacing/plr.py'" >> ~/.zshrc
+fi
+
+if grep -q "alias plr='python3 ~/.pod_local_replacing/plr.py'" ~/.bash_profile; then
+  echo 'bash_profile alias has already been added'
+else
+  echo "" >> ~/.bash_profile
+  echo "alias plr='python3 ~/.pod_local_replacing/plr.py'" >> ~/.bash_profile
+fi

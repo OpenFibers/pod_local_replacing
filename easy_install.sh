8a185b2d3ea5b60b5f76234f55fd092f46d5c414
nanguo.sjh <nanguo.sjh@alibaba-inc.com>
2020-06-20 22:00:15 +0800

    easy install updated

--- easy_install.sh
+++ easy_install.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
-cd ~
-rm -rf '.pod_local_replacing'
-git clone https://github.com/OpenFibers/pod_local_replacing.git pod_local_replacing
-mv pod_local_replacing .pod_local_replacing
-cd -
+#cd ~
+#rm -rf '.pod_local_replacing'
+#git clone https://github.com/OpenFibers/pod_local_replacing.git pod_local_replacing
+#mv pod_local_replacing .pod_local_replacing
+#cd -
 
-if grep -q "alias plr='python3 ~/.pod_local_replacing/plr.py'" ~/.zshrc; then
-  echo 'zshrc alias exist'
-else
-  echo "" >> ~/.zshrc
-  echo "alias plr='python3 ~/.pod_local_replacing/plr.py'" >> ~/.zshrc
-  echo "alias plr_update='bash <(curl -s https://raw.githubusercontent.com/OpenFibers/pod_local_replacing/master/easy_install.sh)'" >> ~/.zshrc
-  echo 'zshrc alias has been added'
-fi
 
-if grep -q "alias plr='python3 ~/.pod_local_replacing/plr.py'" ~/.bash_profile; then
-  echo 'bash_profile alias exist'
-else
-  echo "" >> ~/.bash_profile
-  echo "alias plr='python3 ~/.pod_local_replacing/plr.py'" >> ~/.bash_profile
-  echo "alias plr_update='bash <(curl -s https://raw.githubusercontent.com/OpenFibers/pod_local_replacing/master/easy_install.sh)'" >> ~/.bash_profile
-  echo 'bash_profile alias has been added'
-fi
+add_alias_for_file(){
+  adding_alias=$1
+  file_path=$2
+  echo "adding $adding_alias to $file_path"
+
+  if grep -q "$adding_alias" "$file_path"; then
+    echo "$file_path $adding_alias exist'"
+  else
+    echo "" >> "$file_path"
+    echo "$adding_alias" >> "$file_path"
+    echo "$adding_alias has been added to $file_path"
+  fi
+}
+
+add_alias_for_file "alias plr='python3 ~/.pod_local_replacing/plr.py'" ~/.zshrc
+add_alias_for_file "alias plr_update='bash <(curl -s https://raw.githubusercontent.com/OpenFibers/pod_local_replacing/master/easy_install.sh)'" ~/.zshrc
+add_alias_for_file "alias plr='python3 ~/.pod_local_replacing/plr.py'" ~/.bash_profile
+add_alias_for_file "alias plr_update='bash <(curl -s https://raw.githubusercontent.com/OpenFibers/pod_local_replacing/master/easy_install.sh)'" ~/.bash_profile

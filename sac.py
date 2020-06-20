251e3e242bfda394b6053ca0904378391a89e145
nanguo.sjh <nanguo.sjh@alibaba-inc.com>
2020-06-20 22:31:15 +0800

    sac added

---
+++ sac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+# search [in] all [git] commits
+import sys
+import subprocess
+
+
+def check_keyword(keyword: str) -> None:
+    all_commits: bytes = subprocess.check_output(["git", "rev-list", "--all"])
+    all_commit_string: str = all_commits.decode('utf-8')
+    all_commits_list: [str] = all_commit_string.split('\n')
+    for commit_str in all_commits_list:
+        if len(commit_str) != 0:
+            subprocess.run(["git", "grep", keyword, commit_str])
+    return
+
+
+if __name__ == '__main__':
+    print('Working... very hard... 🙄')
+    for arg in sys.argv[1:]:
+        check_keyword(keyword=arg)
